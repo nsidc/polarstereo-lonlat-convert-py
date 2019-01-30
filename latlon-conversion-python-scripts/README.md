3569b2e2efc8abfa20bc6403fe0173ff4f401a36
Chris Torrence <chris.torrence@nsidc.org>
2019-01-30 14:11:23 -0700

    Clean up docs and code, no functionality changes

--- latlon-conversion-python-scripts/README.md
+++ latlon-conversion-python-scripts/README.md
@@ -1,2 +1,39 @@
-# nsidc_polar_stereo
-NSIDC Polar Stereographic Projection
+# NSIDC Polar Stereographic Projection
+
+https://nsidc.org/data/polar-stereo
+
+NSIDC's polar stereographic projection specifies a projection plane or grid tangent to the Earth's surface at 70° northern and southern latitude. While this increases the distortion at the poles by six percent and decreases the distortion at the grid boundaries by the same amount, the latitude of 70° was selected so that little or no distortion would occur in the marginal ice zone.
+
+This repo contains conversion routines between longitude/latitude and generic x, y (km) coordinates. There are also conversion routines between longitude/latitude and i, j grid coordinates for specific datasets for AMSR-E and SSM/I.
+
+## Python Code
+
+**polar_convert.py**:
+Convert from longitude, latitude to Polar Stereographic x, y (km) and vice versa.  
+`polar_lonlat_to_xy` replaces Fortran `mapll.for`  
+`polar_xy_to_lonlat` replaces Fortran `mapxy.for`  
+
+**nsidc_polar_lonlat.py**:
+Transform from longitude and latitude
+    to NSIDC Polar Stereographic I, J (grid) coordinates.
+
+**nsidc_polar_ij.py**:
+Transform from NSIDC Polar Stereographic I, J (grid) coordinates to longitude and latitude.
+
+**read_mask**: Read in a Polar Stereographic mask file and return the data.
+
+**display_mask**: Read in and display a Polar Stereographic mask file.
+
+## FORTRAN Code
+
+**mapll.for** (_obsolete_): Convert from latitude and longitude to Polar Stereographic x, y (km).
+
+**mapxy.for** (_obsolete_): Convert from Polar Stereographic x, y (km) to latitude and longitude.
+
+**locate.for** (_obsolete_): Transform I,J coordinates of an SSM/I grid cell to latitude and longitude and vice versa.
+
+## IDL Code
+
+**extract_ice.pro** (_obsolete_): Extract sea ice concentrations from Polar Stereographic grid files.
+
+**disp_ssmi_ice_xa.pro** (_obsolete_): Create animations of sea ice concentrations from SSM/I Polar Grids.
--- polar_convert.py
+++ polar_convert.py
@@ -4,7 +4,7 @@ import numpy as np
 def polar_xy_to_lonlat(x, y, true_scale_lat, re, e, hemisphere):
     """Convert from Polar Stereographic (x, y) coordinates to
     geodetic longitude and latitude.
-    
+
     Args:
         x (float): X coordinate(s) in km
         y (float): Y coordinate(s) in km
@@ -12,7 +12,7 @@ def polar_xy_to_lonlat(x, y, true_scale_lat, re, e, hemisphere):
         hemisphere (1 or -1): 1 for Northern hemisphere, -1 for Southern
         re (float): Earth radius in km
         e (float): Earth eccentricity
-    
+
     Returns:
         If x and y are scalars then the result is a
         two-element list containing [longitude, latitude].
@@ -22,26 +22,27 @@ def polar_xy_to_lonlat(x, y, true_scale_lat, re, e, hemisphere):
         the latitudes.
     """
 
-    e2 = e*e
-    slat = true_scale_lat*np.pi/180
-    rho = np.sqrt(x**2 + y**2)
+    e2 = e * e
+    slat = true_scale_lat * np.pi / 180
+    rho = np.sqrt(x ** 2 + y ** 2)
 
-    if abs(true_scale_lat-90.) < 1e-5:
-        t = rho*np.sqrt((1 + e)**(1 + e)*(1 - e)**(1 - e))/(2*re)
+    if abs(true_scale_lat - 90.) < 1e-5:
+        t = rho * np.sqrt((1 + e) ** (1 + e) * (1 - e) ** (1 - e)) / (2 * re)
     else:
-        cm = np.cos(slat)/np.sqrt(1 - e2*(np.sin(slat)**2))
-        t = np.tan((np.pi/4) - (slat/2))/((1 - e*np.sin(slat)) /
-                                          (1 + e*np.sin(slat)))**(e/2)
-        t = rho*t/(re*cm)
+        cm = np.cos(slat) / np.sqrt(1 - e2 * (np.sin(slat) ** 2))
+        t = np.tan((np.pi / 4) - (slat / 2)) / \
+            ((1 - e * np.sin(slat)) / (1 + e * np.sin(slat))) ** (e / 2)
+        t = rho * t / (re * cm)
 
-    chi = (np.pi/2) - 2*np.arctan(t)
-    lat = chi + ((e2/2) + (5*e2**2/24) + (e2**3/12))*np.sin(2*chi) + \
-        ((7*e2**2/48) + (29*e2**3/240))*np.sin(4*chi) + \
-        (7*e2**3/120)*np.sin(6*chi)
-    lat = hemisphere*lat*180/np.pi
-    lon = np.arctan2(hemisphere*x, -hemisphere*y)
-    lon = hemisphere*lon*180/np.pi
-    lon = lon + np.less(lon, 0)*360
+    chi = (np.pi / 2) - 2 * np.arctan(t)
+    lat = chi + \
+        ((e2 / 2) + (5 * e2 ** 2 / 24) + (e2 ** 3 / 12)) * np.sin(2 * chi) + \
+        ((7 * e2 ** 2 / 48) + (29 * e2 ** 3 / 240)) * np.sin(4 * chi) + \
+        (7 * e2 ** 3 / 120) * np.sin(6 * chi)
+    lat = hemisphere * lat * 180 / np.pi
+    lon = np.arctan2(hemisphere * x, -hemisphere * y)
+    lon = hemisphere * lon * 180 / np.pi
+    lon = lon + np.less(lon, 0) * 360
     return [lon, lat]
 
 
@@ -56,7 +57,7 @@ def polar_lonlat_to_xy(longitude, latitude, true_scale_lat, re, e, hemisphere):
         re (float): Earth radius in km
         e (float): Earth eccentricity
         hemisphere (1 or -1): Northern or Southern hemisphere
-    
+
     Returns:
         If longitude and latitude are scalars then the result is a
         two-element list containing [X, Y] in km.
@@ -66,26 +67,26 @@ def polar_lonlat_to_xy(longitude, latitude, true_scale_lat, re, e, hemisphere):
         the Y coordinates.
     """
 
-    lat = abs(latitude)*np.pi/180
-    lon = longitude*np.pi/180
-    slat = true_scale_lat*np.pi/180
+    lat = abs(latitude) * np.pi / 180
+    lon = longitude * np.pi / 180
+    slat = true_scale_lat * np.pi / 180
 
-    e2 = e*e
+    e2 = e * e
 
     # Snyder (1987) p. 161 Eqn 15-9
-    t = np.tan(np.pi/4 - lat/2) / \
-        ((1 - e*np.sin(lat))/(1 + e*np.sin(lat)))**(e/2)
+    t = np.tan(np.pi / 4 - lat / 2) / \
+        ((1 - e * np.sin(lat)) / (1 + e * np.sin(lat))) ** (e / 2)
 
     if abs(90 - true_scale_lat) < 1e-5:
         # Snyder (1987) p. 161 Eqn 21-33
-        rho = 2*re*t/np.sqrt((1 + e)**(1 + e)*(1 - e)**(1 - e))
+        rho = 2 * re * t / np.sqrt((1 + e) ** (1 + e) * (1 - e) ** (1 - e))
     else:
         # Snyder (1987) p. 161 Eqn 21-34
-        tc = np.tan(np.pi/4 - slat/2) / \
-            ((1 - e*np.sin(slat))/(1 + e*np.sin(slat)))**(e/2)
-        mc = np.cos(slat)/np.sqrt(1 - e2*(np.sin(slat)**2))
-        rho = re*mc*t/tc
+        tc = np.tan(np.pi / 4 - slat / 2) / \
+            ((1 - e * np.sin(slat)) / (1 + e * np.sin(slat))) ** (e / 2)
+        mc = np.cos(slat) / np.sqrt(1 - e2 * (np.sin(slat) ** 2))
+        rho = re * mc * t / tc
 
-    x = rho*hemisphere*np.sin(hemisphere*lon)
-    y = -rho*hemisphere*np.cos(hemisphere*lon)
+    x = rho * hemisphere * np.sin(hemisphere * lon)
+    y = -rho * hemisphere * np.cos(hemisphere * lon)
     return [x, y]
