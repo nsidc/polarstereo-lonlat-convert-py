49548b353d0333f62c57ae30bcd67c09a4338a6d
Trey Stafford <trey.stafford@nsidc.org>
2021-09-15 10:41:28 -0600

    Run tests when the `test_nsidc_polar.py` is run directly

--- lonlat-conversion-python/test_nsidc_polar.py
+++ lonlat-conversion-python/test_nsidc_polar.py
@@ -15,29 +15,30 @@ import numpy as np
 from nsidc_polar_ij import nsidc_polar_ij
 from nsidc_polar_lonlat import nsidc_polar_lonlat
     
-grid = [6.25, 12.5, 25]
-imax = [1216, 608, 304]
-jmax = [1792, 896, 448]
-for g in [0, 1, 2]:
-    print("Testing Northern hemisphere, " + str(grid[g]) + "km")
-    tic = time.perf_counter()
-    for i in range(1, imax[g]):
-        jj = np.arange(1, jmax[g])
-        lonlat = nsidc_polar_ij(i, jj, grid[g], 1)
-        ij = nsidc_polar_lonlat(lonlat[0], lonlat[1], grid[g], 1)
-        if np.any(np.not_equal(ij[0], i)) or np.any(np.not_equal(ij[1], jj)):
-            raise RuntimeError("error: i=" + str(i))
-    print(" time=" + str(time.perf_counter() - tic))
+if __name__ == '__main__':
+    grid = [6.25, 12.5, 25]
+    imax = [1216, 608, 304]
+    jmax = [1792, 896, 448]
+    for g in [0, 1, 2]:
+        print("Testing Northern hemisphere, " + str(grid[g]) + "km")
+        tic = time.perf_counter()
+        for i in range(1, imax[g]):
+            jj = np.arange(1, jmax[g])
+            lonlat = nsidc_polar_ij(i, jj, grid[g], 1)
+            ij = nsidc_polar_lonlat(lonlat[0], lonlat[1], grid[g], 1)
+            if np.any(np.not_equal(ij[0], i)) or np.any(np.not_equal(ij[1], jj)):
+                raise RuntimeError("error: i=" + str(i))
+        print(" time=" + str(time.perf_counter() - tic))
 
-imax = [1264, 632, 316]
-jmax = [1328, 664, 332]
-for g in [0, 1, 2]:
-    print("Testing Southern hemisphere, " + str(grid[g]) + "km")
-    tic = time.perf_counter()
-    for i in range(1, imax[g]):
-        jj = np.arange(1, jmax[g])
-        lonlat = nsidc_polar_ij(i, jj, grid[g], -1)
-        ij = nsidc_polar_lonlat(lonlat[0], lonlat[1], grid[g], -1)
-        if np.any(np.not_equal(ij[0], i)) or np.any(np.not_equal(ij[1], jj)):
-            raise RuntimeError("error: i=" + str(i))
-    print(" time=" + str(time.perf_counter() - tic))
+    imax = [1264, 632, 316]
+    jmax = [1328, 664, 332]
+    for g in [0, 1, 2]:
+        print("Testing Southern hemisphere, " + str(grid[g]) + "km")
+        tic = time.perf_counter()
+        for i in range(1, imax[g]):
+            jj = np.arange(1, jmax[g])
+            lonlat = nsidc_polar_ij(i, jj, grid[g], -1)
+            ij = nsidc_polar_lonlat(lonlat[0], lonlat[1], grid[g], -1)
+            if np.any(np.not_equal(ij[0], i)) or np.any(np.not_equal(ij[1], jj)):
+                raise RuntimeError("error: i=" + str(i))
+        print(" time=" + str(time.perf_counter() - tic))
