8f5fed8711d8f2a308abbdcdcf5c7ccf9e829c46
Trey Stafford <trey.stafford@nsidc.org>
2021-09-16 10:49:55 -0600

    Rename `grid` arg -> `grid_size`

--- lonlat-conversion-python/nsidc_polar_ij.py
+++ lonlat-conversion-python/nsidc_polar_ij.py
@@ -3,14 +3,14 @@ import numpy as np
 from polar_convert import polar_xy_to_lonlat
 
 
-def nsidc_polar_ij(i, j, grid, hemisphere):
+def nsidc_polar_ij(i, j, grid_size, hemisphere):
     """Transform from NSIDC Polar Stereographic I, J coordinates
     to longitude and latitude coordinates
     
     Args:
-        i (int): an integer or integer array giving the x grid coordinate(s)
-        j (int): an integer or integer array giving the y grid coordinate(s)
-        grid (float): 6.25, 12.5 or 25; the grid cell dimensions in km
+        i (int): an integer or integer array giving the x grid_size coordinate(s)
+        j (int): an integer or integer array giving the y grid_size coordinate(s)
+        grid_size (float): 6.25, 12.5 or 25; the grid_size cell dimensions in km
         hemisphere (1 or -1): Northern or Southern hemisphere
     
     Returns:
@@ -30,8 +30,8 @@ def nsidc_polar_ij(i, j, grid, hemisphere):
     re = 6378.273
     e = 0.081816153
 
-    if grid != 6.25 and grid != 12.5 and grid != 25:
-        raise ValueError("Legal grid values are 6.25, 12.5, or 25")
+    if grid_size != 6.25 and grid_size != 12.5 and grid_size != 25:
+        raise ValueError("Legal grid_size values are 6.25, 12.5, or 25")
     
     if hemisphere != 1 and hemisphere != -1:
         raise ValueError("Legal hemisphere values are 1 or -1")
@@ -40,19 +40,19 @@ def nsidc_polar_ij(i, j, grid, hemisphere):
         delta = 45
         imax = 1216
         jmax = 1792
-        xmin = -3850 + grid/2
-        ymin = -5350 + grid/2
+        xmin = -3850 + grid_size/2
+        ymin = -5350 + grid_size/2
     else:
         delta = 0
         imax = 1264
         jmax = 1328
-        xmin = -3950 + grid/2
-        ymin = -3950 + grid/2
+        xmin = -3950 + grid_size/2
+        ymin = -3950 + grid_size/2
 
-    if grid == 12.5:
+    if grid_size == 12.5:
         imax = imax//2
         jmax = jmax//2
-    elif grid == 25:
+    elif grid_size == 25:
         imax = imax//4
         jmax = jmax//4
 
@@ -62,8 +62,8 @@ def nsidc_polar_ij(i, j, grid, hemisphere):
         raise ValueError("'j' value is out of range: [1, " + str(jmax) + "]")
 
     # Convert I, J pairs to x and y distances from origin.
-    x = ((i - 1)*grid) + xmin
-    y = ((jmax - j)*grid) + ymin
+    x = ((i - 1)*grid_size) + xmin
+    y = ((jmax - j)*grid_size) + ymin
     lonlat = polar_xy_to_lonlat(x, y, true_scale_lat, re, e, hemisphere)
     lon = lonlat[0] - delta
     lon = lon + np.less(lon, 0)*360
