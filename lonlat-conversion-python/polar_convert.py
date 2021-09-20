c9746f80598d9bc857892aa0da835251a38d7fb1
Trey Stafford <trey.stafford@nsidc.org>
2021-09-20 13:42:05 -0600

    Rename python functions to be more descriptive
    & move all functions into `polar_convert.py`.

--- lonlat-conversion-python/polar_convert.py
+++ lonlat-conversion-python/polar_convert.py
@@ -1,6 +1,12 @@
 import numpy as np
 
-from validators import validate_hemisphere
+from constants import (
+    EARTH_ECCENTRICITY,
+    EARTH_RADIUS_KM,
+    NORTH,
+    TRUE_SCALE_LATITUDE,
+)
+from validators import validate_hemisphere, validate_grid_size
 
 
 def _hemi_direction(hemisphere):
@@ -103,3 +109,132 @@ def polar_lonlat_to_xy(longitude, latitude, true_scale_lat, re, e, hemisphere):
     x = rho * hemi_direction * np.sin(hemi_direction * lon)
     y = -rho * hemi_direction * np.cos(hemi_direction * lon)
     return [x, y]
+
+
+def polar_ij_to_lonlat(i, j, grid_size, hemisphere):
+    """Transform from NSIDC Polar Stereographic I, J coordinates
+    to longitude and latitude coordinates
+
+    Args:
+        i (int): an integer or integer array giving the x grid_size coordinate(s)
+        j (int): an integer or integer array giving the y grid_size coordinate(s)
+        grid_size (float): 6.25, 12.5 or 25; the grid_size cell dimensions in km
+        hemisphere ('north' or 'south'): Northern or Southern hemisphere
+
+    Returns:
+        If i and j are scalars then the result is a
+        two-element list containing [longitude, latitude].
+        If i and j are numpy arrays then the result will be a two-element
+        list where the first element is a numpy array containing
+        the longitudes and the second element is a numpy array containing
+        the latitudes.
+
+    Examples:
+        print(nsidc_polar_ij(608, 896, 12.5, 1))
+            [350.01450147320855, 34.40871032516291]
+    """
+
+    validate_grid_size(grid_size)
+    hemisphere = validate_hemisphere(hemisphere)
+
+    if hemisphere == NORTH:
+        delta = 45
+        imax = 1216
+        jmax = 1792
+        xmin = -3850 + grid_size / 2
+        ymin = -5350 + grid_size / 2
+    else:
+        delta = 0
+        imax = 1264
+        jmax = 1328
+        xmin = -3950 + grid_size / 2
+        ymin = -3950 + grid_size / 2
+
+    if grid_size == 12.5:
+        imax = imax // 2
+        jmax = jmax // 2
+    elif grid_size == 25:
+        imax = imax // 4
+        jmax = jmax // 4
+
+    if np.any(np.less(i, 1)) or np.any(np.greater(i, imax)):
+        raise ValueError("'i' value is out of range: [1, " + str(imax) + "]")
+    if np.any(np.less(j, 1)) or np.any(np.greater(j, jmax)):
+        raise ValueError("'j' value is out of range: [1, " + str(jmax) + "]")
+
+    # Convert I, J pairs to x and y distances from origin.
+    x = ((i - 1) * grid_size) + xmin
+    y = ((jmax - j) * grid_size) + ymin
+    lon, lat = polar_xy_to_lonlat(
+        x,
+        y,
+        TRUE_SCALE_LATITUDE,
+        EARTH_RADIUS_KM,
+        EARTH_ECCENTRICITY,
+        hemisphere
+    )
+    lon = lon - delta
+    lon = lon + np.less(lon, 0) * 360
+
+    return [lon, lat]
+
+
+def polar_lonlat_to_ij(longitude, latitude, grid_size, hemisphere):
+    """Transform from geodetic longitude and latitude coordinates
+    to NSIDC Polar Stereographic I, J coordinates
+
+    Args:
+        longitude (float): longitude or longitude array in degrees
+        latitude (float): latitude or latitude array in degrees (positive)
+        grid_size (float): 6.25, 12.5 or 25; the grid_size cell dimensions in km
+        hemisphere ('north' or 'south'): Northern or Southern hemisphere
+
+    Returns:
+        If longitude and latitude are scalars then the result is a
+        two-element list containing [I, J].
+        If longitude and latitude are numpy arrays then the result will
+        be a two-element list where the first element is a numpy array for
+        the I coordinates and the second element is a numpy array for
+        the J coordinates.
+
+    Examples:
+        print(nsidc_polar_lonlat(350.0, 34.41, 12.5, 1))
+            [608, 896]
+    """
+
+    validate_grid_size(grid_size)
+    hemisphere = validate_hemisphere(hemisphere)
+
+    if hemisphere == NORTH:
+        delta = 45
+        imax = 1216
+        jmax = 1792
+        xmin = -3850 + grid_size / 2
+        ymin = -5350 + grid_size / 2
+    else:
+        delta = 0
+        imax = 1264
+        jmax = 1328
+        xmin = -3950 + grid_size / 2
+        ymin = -3950 + grid_size / 2
+
+    if grid_size == 12.5:
+        imax = imax // 2
+        jmax = jmax // 2
+    elif grid_size == 25:
+        imax = imax // 4
+        jmax = jmax // 4
+
+    x, y = polar_lonlat_to_xy(
+        longitude + delta,
+        np.abs(latitude),
+        TRUE_SCALE_LATITUDE,
+        EARTH_RADIUS_KM,
+        EARTH_ECCENTRICITY,
+        hemisphere
+    )
+    i = (np.round((x - xmin) / grid_size)).astype(int) + 1
+    j = (np.round((y - ymin) / grid_size)).astype(int) + 1
+    # Flip grid_size orientation in the 'y' direction
+    j = jmax - j + 1
+    return [i, j]
